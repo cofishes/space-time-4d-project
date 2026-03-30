980358eae06dc33030475b3cd00db64103016759
cofishes <402741056@qq.com>
2026-03-30 14:29:41 +0800

    Add quickstart demo script

---
+++ quickstart.py
@@ -0,0 +1,16 @@
+# Quickstart demo script
+
+# Import necessary libraries
+import numpy as np
+
+# Quick demo function
+def quick_demo():
+    print("Current Date and Time (UTC): 2026-03-30 06:28:29")
+    # Here you can add more logic for the demo
+    # For now, we just showcase a simple numpy operation
+    arr = np.array([1, 2, 3, 4, 5])
+    print(f"Sum: {np.sum(arr)}")
+
+# Execute demo function
+if __name__ == '__main__':
+    quick_demo()
